57ceb02d7f8f82566b425a200b65068a9815a088
Marko Djuricic <markodjuricic@Markos-MacBook-Pro.local>
2021-03-15 18:48:39 +0100

    zadatak 2

---
+++ create-post.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="../../../../favicon.ico">
+    <title>Vivify Blog - Create Post</title>
+
+    <!-- Bootstrap core CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
+
+    <!-- Custom styles for this template -->
+    <link href="styles/blog.css" rel="stylesheet">
+</head>
+
+<body>
+    <?php include_once('header.php'); ?>
+    <?php include_once('sidebar.php'); ?>
+    <?php include_once('footer.php'); ?>
+</body>
+
+</html>
